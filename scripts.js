2d6d4c3c90389da88294e12e1e017476ee3075f0
Clicker <unkownmalesir@gmail.com>
2025-03-02 20:14:17 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -13,20 +13,31 @@ document.addEventListener('DOMContentLoaded', (event) => {
             case 'ArrowUp':
             case 'w':
                 posY -= speed;
+                character.classList.remove('idle');
+                character.classList.add('walking');
                 break;
             case 'ArrowDown':
             case 's':
                 posY += speed;
+                character.classList.remove('idle');
+                character.classList.add('walking');
                 break;
             case 'ArrowLeft':
             case 'a':
                 posX -= speed;
+                character.classList.remove('idle');
+                character.classList.add('walking');
                 break;
             case 'ArrowRight':
             case 'd':
                 posX += speed;
+                character.classList.remove('idle');
+                character.classList.add('walking');
                 break;
-        }
-        moveCharacter();
-    });
-});
+            case ' ':
+                character.classList.remove('idle');
+                character.classList.add('jumping');
+                break;
+            case 'c':
+                character.classList.add('charging');
+                document.getElementById('rocks').style
